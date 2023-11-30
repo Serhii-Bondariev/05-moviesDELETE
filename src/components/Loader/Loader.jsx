d708b9a779f30dd4daa1ffea4ff46afcc6c7ba22
Serhii <xx.code4u@gmail.com>
2023-11-30 15:03:22 +0200

    Fix loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,23 +1,40 @@
-import { Blocks } from 'react-loader-spinner';
+// import { Blocks } from 'react-loader-spinner';
+
+import styles from './Loader.module.css';
 import React from 'react';
+
+// const Loader = () => {
+//   return (
+//     <Blocks
+//       visible={true}
+//       height="80"
+//       width="80"
+//       ariaLabel="blocks-loading"
+//       wrapperStyle={{
+//         position: 'absolute',
+//         top: '50%',
+//         left: '50%',
+//         transform: 'translate(-50%, -50%)',
+//         zIndex: 9999,
+//       }}
+//       wrapperClass="blocks-wrapper"
+//     />
+//   );
+// };
 
 const Loader = () => {
   return (
-    <Blocks
-      visible={true}
-      height="80"
-      width="80"
-      ariaLabel="blocks-loading"
-      wrapperStyle={{
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        zIndex: 9999,
-      }}
-      wrapperClass="blocks-wrapper"
-    />
+    <div>
+      <div className={styles.center}>
+  <div className={styles.ring}></div>
+  <span className={styles.loading}>Loading...</span>
+</div>
+
+      
+    </div>
+    
+    
   );
-};
+}
 
 export default Loader;
